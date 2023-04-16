9c4f5d8576cd226d2de80882a1d121bdf31766d1
Walter S L Junior <wjunino10@hotmail.com>
2023-04-16 16:22:15 -0300

    1º função

---
+++ HttpTrigger1/index.js
@@ -0,0 +1,28 @@
+module.exports = async function (context, req) {
+    const num = req.query.num;
+    let frase;
+
+    switch (num) {
+        case '1':
+            frase = 'A vida é como uma montanha-russa, com altos e baixos';
+            break;
+        case '2':
+            frase = 'Esta é a frase 2.';
+            break;
+        case '3':
+            frase = 'Esta é a frase 3.';
+            break;
+        case '4':
+            frase = 'Esta é a frase 4.';
+            break;
+        case '5':
+            frase = 'Esta é a frase 5.';
+            break;
+        default:
+            frase = 'Número inválido.';
+    }
+
+    context.res = {
+        body: frase
+    };
+};
