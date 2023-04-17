4f15d3065d0d50fb593cb85ecb9b8240d8d28e39
Walter S L Junior <wjunino10@hotmail.com>
2023-04-16 21:34:20 -0300

    Demo pronto

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,25 +1,30 @@
 module.exports = async function (context, req) {
-    const num = req.query.num;
+
+    let num = req.query.num;
     let frase;
 
-    switch (num) {
-        case '1':
-            frase = 'A vida é como uma montanha-russa, com altos e baixos';
-            break;
-        case '2':
-            frase = 'Mae muki ni ne. – Mantenha-se positivo.';
-            break;
-        case '3':
-            frase = 'Nada Detem a macha inexorável do tempo';
-            break;
-        case '4':
-            frase = 'Imagine uma nova história para sua vida e acredite nela.';
-            break;
-        case '5':
-            frase = 'Esta é a frase 5.';
-            break;
-        default:
-            frase = 'Número inválido.';
+    if (num) {
+        switch (num) {
+            case '1':
+                frase = 'A vida é como uma montanha-russa, com altos e baixos';
+                break;
+            case '2':
+                frase = 'Mae muki ni ne. – Mantenha-se positivo.';
+                break;
+            case '3':
+                frase = 'Nada Detem a macha inexorável do tempo';
+                break;
+            case '4':
+                frase = 'Imagine uma nova história para sua vida e acredite nela.';
+                break;
+            case '5':
+                frase = 'Esta é a frase 5.';
+                break;
+            default:
+                frase = 'Némero invalido';
+        }
+    } else {
+        frase = 'Digite um número de 1 a 5 acrescentando ?num=número na URL';
     }
 
     context.res = {
